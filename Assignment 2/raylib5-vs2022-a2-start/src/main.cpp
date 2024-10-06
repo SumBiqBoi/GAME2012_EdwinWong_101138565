4e1107f3cd94bda50cba207db427b83f2fef9403
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2024-10-05 20:33:44 -0400

    Added assignment 2. Points not updating to new points so cannot draw smaller squares

---
+++ Assignment 2/raylib5-vs2022-a2-start/src/main.cpp
@@ -0,0 +1,87 @@
+#include "raylib.h"
+#include "Math.h"
+
+// Effectively GL_LINE_LOOP
+void DrawLineLoop(Vector2* points, int pointCount, Color color, int revAmount)
+{
+    float thickness = 10.0f;
+    for (int i = 0; i < pointCount; i++)
+    {
+        // Verify the values of curr and next in the debugger to help understand!
+        int curr = i;
+        int next = (i + 1) % pointCount;
+        Vector2 A = points[curr];
+        Vector2 B = points[next];
+        DrawLineEx(A, B, thickness, color); 
+    }
+}
+
+void DrawLineLoopAuto(Vector2* points, int pointCount, Color color, int createAmount)
+{
+    float thickness = 5.0f;
+    for (int k = 0; k < createAmount; k++)
+    {
+        for (int i = 0; i < pointCount; i++)
+        {
+            int curr = i;
+            int next = (i + 1) % pointCount;
+            Vector2 A = points[curr];
+            Vector2 B = points[next];
+            DrawLineEx(A, B, thickness, color);
+            for (int l = 0; l < pointCount; l++)
+            {
+                Vector2 C = (points[curr] + points[next]) * 0.5f;
+                Vector2 D = (points[next] + points[next + 1]) * 0.5f;
+                DrawLineEx(C, D, thickness, color);
+            }
+        }
+    }
+}
+
+int main()
+{
+    InitWindow(800, 800, "Game");
+    SetTargetFPS(60);
+    float w = GetScreenWidth();
+    float h = GetScreenHeight();
+    float tc = 10.0f;
+
+    Vector2 curr[4]
+    {
+        { 0.0f, 0.0f }, // top-left
+        { w, 0.0f },    // top-right
+        { w, h },       // bot-right
+        { 0.0f, h }     // bot-left
+    };
+
+    Vector2 next[4]
+    {
+        (curr[0] + curr[1]) * 0.5f,
+        (curr[1] + curr[2]) * 0.5f,
+        (curr[2] + curr[3]) * 0.5f,
+        (curr[3] + curr[0]) * 0.5f
+    };
+
+    Vector2 next1[4]
+    {
+        (next[0] + next[1]) * 0.5f,
+        (next[1] + next[2]) * 0.5f,
+        (next[2] + next[3]) * 0.5f,
+        (next[3] + next[0]) * 0.5f
+    };
+
+    // **Consider prototyping the automatic (loop-based) vertex generation here before going to OpenGL!**
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Hello World!", 10, 10, 20, GRAY);
+        //DrawLineLoop(curr, 4, VIOLET, 1);
+        //DrawLineLoop(next, 4, LIME);
+        DrawLineLoopAuto(curr, 4, VIOLET, 5);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
